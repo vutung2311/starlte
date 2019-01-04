742df22eddeffa4047a2d2c492635fe7167a7ef8
Tung Vu <vutung.ct@gmail.com>
2019-01-04 10:35:27 +0700

    driver: usb: modify mass_storage gadget to work with configfs

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -233,6 +233,8 @@ static const char fsg_string_interface[] = "Mass Storage";
 #include "storage_common.h"
 #include "f_mass_storage.h"
 
+extern struct device *create_function_device(char *name);
+
 /* Static strings, in UTF-8 (for simplicity we use only ASCII characters) */
 static struct usb_string		fsg_strings[] = {
 	{FSG_STRING_INTERFACE,		fsg_string_interface},
@@ -3476,6 +3478,10 @@ static struct usb_function_instance *fsg_alloc_inst(void)
 	config_group_init_type_name(&opts->lun0.group, "lun.0", &fsg_lun_type);
 	configfs_add_default_group(&opts->lun0.group, &opts->func_inst.group);
 
+	dev = create_function_device("f_mass_storage");
+	if (IS_ERR(dev))
+		return PTR_ERR(dev);
+
 	return &opts->func_inst;
 
 release_buffers:
@@ -3514,7 +3520,7 @@ static struct usb_function *fsg_alloc(struct usb_function_instance *fi)
 	opts->refcnt++;
 	mutex_unlock(&opts->lock);
 
-	fsg->function.name	= FSG_DRIVER_DESC;
+	fsg->function.name	= "mass_storage";
 	fsg->function.bind	= fsg_bind;
 	fsg->function.unbind	= fsg_unbind;
 	fsg->function.setup	= fsg_setup;
